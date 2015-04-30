f2eda2be4f3424026d535cab796759bb2219631f
pinkylab <order66.rgxa2@gmail.com>
2015-04-30 18:46:03 +0900

    UA指定した。ほんとにできてるのか不明

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -8,6 +8,8 @@ using System.Net;
 
 public partial class _Default : Page
 {
+    const string UA_iOS7_1_2 = "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53";
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -17,9 +19,13 @@ public partial class _Default : Page
         string source_url = TextBoxURL.Text;
         string _html = "";
         WebClient wc = new WebClient();
-        //UTF-8ならこれ1行でもOK
+
+        // ユーザーエージェント指定
+        wc.Headers.Add("User-Agent", UA_iOS7_1_2);
+
         if (source_url != null)
         {
+            //UTF-8ならこれ1行でもOK
             _html = wc.DownloadString(source_url);
         }
 
