55f98f17c2e8c664c73123423bc3e4505cf3f107
pinkylab <order66.rgxa2@gmail.com>
2015-05-01 16:09:53 +0900

    クソみたいなエラー処理追加

--- Default.aspx
+++ Default.aspx
@@ -1,26 +1,28 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Title="エロ動画を観よう" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
 <asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
     <section class="featured">
         <div class="content-wrapper">
             <hgroup class="title">
-                <h1><%: Title %>.</h1>
-                <h2>Modify this template to jump-start your ASP.NET application.</h2>
+                <h1><%: Title %></h1>
+                <h2>超便利エロ動画ゲッチュツール</h2>
             </hgroup>
-            <p>
-                To learn more about ASP.NET, visit <a href="http://asp.net" title="ASP.NET Website">http://asp.net</a>. 
-                The page features <mark>videos, tutorials, and samples</mark> to help you get the most from 
-                ASP.NET. If you have any questions about ASP.NET visit 
-                <a href="http://forums.asp.net/18.aspx" title="ASP.NET Forum">our forums</a>.
-            </p>
         </div>
     </section>
 </asp:Content>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h2>使い方</h2>
+    <p>下のテキストボックスにxvideosの動画URLを入れてください。「エロ動画をゲットする」ボタンを押すと、エロ動画へのリンクが表示されます。</p>
+    <p>あとは煮るなり焼くなり好きにしてください。右クリックから「名前をつけて保存（ブラウザによって若干表記は違う）」で動画の保存ができます。</p>
     <h3>URLを入れてね</h3>
     <asp:TextBox ID="TextBoxURL" runat="server" Width="1000px"></asp:TextBox>
-    <asp:Button ID="ButtonAnalyze" runat="server" Text="Button" OnClick="ButtonAnalyze_Click" />
-    <asp:TextBox ID="TextBoxResult" runat="server" Height="500px" TextMode="MultiLine" Width="1500px"></asp:TextBox>
-    <asp:HyperLink ID="HyperLinkResult" runat="server">HyperLink</asp:HyperLink>
+    <asp:Button ID="ButtonAnalyze" runat="server" Text="エロ動画をゲットする" OnClick="ButtonAnalyze_Click" />
+    <h3>エロ動画への直リンク</h3>
+    <asp:HyperLink ID="HyperLinkResult" runat="server"></asp:HyperLink>
+    <h3>エラーログ</h3>
+    <p>catchしたものを直接表示してるクソアンドクソ</p>
+    <p>エラーが返ってくる場合は基本的に指定されたURLが間違ってる。</p>
+    <asp:TextBox ID="TextBoxResult" runat="server" Height="250px" TextMode="MultiLine" Width="1000px"></asp:TextBox>
+    
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,57 +16,70 @@ public partial class _Default : Page
     }
     protected void ButtonAnalyze_Click(object sender, EventArgs e)
     {
-        string source_url = TextBoxURL.Text;
-        string _html = "";
-        WebClient wc = new WebClient();
-
-        // ユーザーエージェント指定
-        wc.Headers.Add("User-Agent", UA_iOS7_1_2);
-
-        if (source_url != null)
+        string strResult = "動画URLの取得に性交しました。";
+        try
         {
-            // UTF-8ならこれ1行でもOK
-            // 例外処理書く
-            _html = wc.DownloadString(source_url);
-        }
+            string source_url = TextBoxURL.Text;
+            string _html = "";
+            WebClient wc = new WebClient();
 
-        // mobileReplacePlayerDivTwoQual関数の引数にmp4形式の動画URLの直リンクがある
-        // 引数部分のみを正規表現で抜き出す
-        System.Text.RegularExpressions.Regex r = 
-            new System.Text.RegularExpressions.Regex(
-@"mobileReplacePlayerDivTwoQual\((?<hikisu>.*)\)",
-System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            // ユーザーエージェント指定
+            wc.Headers.Add("User-Agent", UA_iOS7_1_2);
 
-        System.Text.RegularExpressions.Match m = r.Match(_html);
-
-        string hikisu = "";
-        while (m.Success)
-        {
-            //一致した対象が見つかったときキャプチャした部分文字列を表示
-            hikisu = m.Groups["hikisu"].Value;
-
-            m = m.NextMatch();
-        }
-
-        // 引数はカンマ区切りなので、カンマで分割する
-        string[] strArr = hikisu.Split(',');
-
-        int i = 0;
-        foreach (string str in strArr)
-        {
-            // 文字列に「/mp4/」って文字があったら動画URLとする
-            if (str.Contains(@"/mp4/"))
+            if (source_url != null)
             {
-                break;
+                // UTF-8ならこれ1行でもOK
+                // 例外処理書く
+                _html = wc.DownloadString(source_url);
             }
-            ++i;
-        }
 
-        // 前後の空白を消した後、シングルクォートも消す
-        string resUrl = strArr[i].Trim().Trim('\'');
-        HyperLinkResult.Text = resUrl;
-        HyperLinkResult.NavigateUrl = resUrl;
-        HyperLinkResult.Target = "_blank";
-        TextBoxResult.Text = resUrl;
+            // mobileReplacePlayerDivTwoQual関数の引数にmp4形式の動画URLの直リンクがある
+            // 引数部分のみを正規表現で抜き出す
+            System.Text.RegularExpressions.Regex r =
+                new System.Text.RegularExpressions.Regex(
+    @"mobileReplacePlayerDivTwoQual\((?<hikisu>.*)\)",
+    System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            System.Text.RegularExpressions.Match m = r.Match(_html);
+
+            string hikisu = "";
+            while (m.Success)
+            {
+                //一致した対象が見つかったときキャプチャした部分文字列を表示
+                hikisu = m.Groups["hikisu"].Value;
+
+                m = m.NextMatch();
+            }
+
+            // 引数はカンマ区切りなので、カンマで分割する
+            string[] strArr = hikisu.Split(',');
+
+            int i = 0;
+            foreach (string str in strArr)
+            {
+                // 文字列に「/mp4/」って文字があったら動画URLとする
+                if (str.Contains(@"/mp4/"))
+                {
+                    break;
+                }
+                ++i;
+            }
+
+            // 前後の空白を消した後、シングルクォートも消す
+            string resUrl = strArr[i].Trim().Trim('\'');
+            // 結果の表示
+            HyperLinkResult.Text = resUrl;
+            HyperLinkResult.NavigateUrl = resUrl;
+            HyperLinkResult.Target = "_blank";
+        }
+        catch(Exception ex)
+        {
+            // あばばばばばばばばばばば
+            strResult = ex.ToString();
+        }
+        finally
+        {
+            TextBoxResult.Text = strResult;
+        }
     }
 }
